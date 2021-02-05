e082492c0b7b4436ac323d14976ac1106a1088f9
Sal Gamez <gamezsalvador@gmail.com>
2021-02-04 21:27:31 -0700

    added the folders in src file for pages, components and utils for proper layouts and started coding the pages and components js files

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "../../components/Search";
+
+function Header() {
+  return (
+    <div className="container-fluid text-center bg-primary border-bottom border-danger border-thick">
+      <h1>Employee Directory</h1>
+      <br />
+      <Search />
+    </div>
+  );
+}
