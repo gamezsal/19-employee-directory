e19acd1d160c433b7cd4dfd0ab8b4ce251a6f586
Sal Gamez <gamezsalvador@gmail.com>
2021-02-11 19:54:33 -0700

    added Table index.js file

---
+++ src/components/Table/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Table(props) {
+    const { results } = props
+
+    return (
+        <table className="table">
+            <thead className="thead-dark">
+                <tr>
+                    <th scope="col">
+                        <button type="button">
+                            Name
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button">
+                            Date of Birth
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button">
+                            Email
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button">
+                            Phone
+                        </button>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {results?.map((result, index) => (
+                    <TableBody result={result} index={index} />
+                ))}
+            </tbody>
+        </table>
+    )
+}
